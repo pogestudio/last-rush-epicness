3b0ce5f5dd5256894efe05bb6dec90909641d4bf
CA Ewerbring <caewerbring@gmail.com>
2013-11-24 00:18:10 +0100

    Enabled simply shooting with prefabricates.

---
+++ prototype/Assets/firingBullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class firingBullet : MonoBehaviour
+{
+
+    public Rigidbody projectile;
+    public int speed = 20;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown (0)) {
+            Rigidbody newProjectile = Instantiate (projectile, transform.position, transform.rotation) as Rigidbody;
+            newProjectile.velocity = transform.TransformDirection (Vector3.forward * speed);
+            Debug.Log (Vector3.forward * speed);
+            Destroy (newProjectile.gameObject, 10);
+        }
+    }
+
+}
---
+++ prototype/Assets/projectileCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectileCollision : MonoBehaviour
+{
+
+    public int damage = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (damage == 0)
+            Debug.Log ("Damage is 0 on a projectile, fix!");
+    }
+	
+    void OnCollisionEnter (Collision collision)
+    {
+        GameObject target = collision.gameObject;
+        target.transform.SendMessage ("applyDamage", damage, SendMessageOptions.DontRequireReceiver);
+        
+
+        Destroy (gameObject);
+    }
+}
