68b8624aeed745bb793cfec87ff5ccf2d430fe2d
CA Ewerbring <caewerbring@gmail.com>
2013-11-23 17:39:17 +0100

    Added WASD movements, as well as a top down camera that follows player

---
+++ prototype/Assets/TopDownCamera.js
@@ -0,0 +1,12 @@
+﻿var target : Transform;
+var distance : float;
+var height : float;
+
+function Update(){
+ 
+    transform.position.z = target.position.z -distance;
+    transform.position.y = target.position.y + height;
+    transform.position.x = target.position.x;
+    transform.LookAt(target.position);
+ 
+}
