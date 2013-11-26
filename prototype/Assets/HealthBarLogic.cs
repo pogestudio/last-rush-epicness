060831bd8ca021a3e93215f8e0a88c2410903cae
CA Ewerbring <caewerbring@gmail.com>
2013-11-26 14:09:24 +0100

    Added health bar

---
+++ prototype/Assets/HealthBarLogic.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Health bar logic.
+/// Has the responsibility to correctly display the amount of lives to the user.
+/// Should be added to its own gameobject, as a component
+/// 
+/// </summary>
+
+public class HealthBarLogic : MonoBehaviour
+{	
+	//the image that will be shown
+	public Texture2D lifeImage;
+	private int lives = 0;
+	
+	//can be modified to display the life in a special fashion. 
+	public int startingX;
+	public int startingY;
+	public int widthPadding;
+		
+	//used for internal purposes.
+	private int lifeImageWidth;
+	private int lifeImageHeight;
+	
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		lifeImageWidth = lifeImage.width;
+		lifeImageHeight = lifeImage.height;
+		//Debug.Log ("life image width and height:: " + lifeImageWidth + "    " + lifeImageHeight);
+	}
+	
+	void OnGUI ()
+	{
+		//we have to store the default value. otherwise all gui elements will be rendered
+		//with transparant backgrounds.
+		Color defaultColor = GUI.backgroundColor;
+		GUI.backgroundColor = Color.clear;
+		
+		//simply draw the same amount of boxes as we have lives.
+		for (int i = 0; i < lives; i++) {
+			int x = startingX + i * (lifeImageWidth + widthPadding);
+			int y = startingY;
+			GUI.Box (new Rect (x, y, lifeImageWidth, lifeImageHeight), lifeImage);	
+		}
+		
+		GUI.backgroundColor = defaultColor;
+		
+	}
+	
+	/// <summary>
+	/// Set the total amount of life that should be shown.
+	/// </summary>
+	/// <param name="amount">Amount.</param>
+	public void setLife (int amount)
+	{
+		lives = Mathf.Max (0, amount);
+	}
+}
--- prototype/Assets/Scripts/PlayerHealth.cs
+++ prototype/Assets/Scripts/PlayerHealth.cs
@@ -3,15 +3,30 @@ using System.Collections;
 
 /*
 
-This is player logic, and decides all player related behaviour.
-health - the health of the player
+This script takes care of all the health related stuff of the player.
+Including taking damage, killing character, despawning character, and adding lives.
+
+health - the starting of the player
 
 */
 
 public class PlayerHealth : MonoBehaviour
 {
 	public int health = 3;
+	
+	private HealthBarLogic healthBar;
 
+	void Start ()
+	{
+		//Find the gameobject with the healthbarlogic and assign at start.
+		//It should be tagged with "HealthBar". 
+		GameObject healthBarContainer = GameObject.FindGameObjectWithTag ("HealthBar");
+		healthBar = healthBarContainer.GetComponent<HealthBarLogic> () as HealthBarLogic;
+		if (!healthBar)
+			Debug.LogError ("FAIL! Need a logic script for health bar!");
+		healthBar.setLife (health); //set the initial life.
+	
+	}
 	private void die ()
 	{
 		Debug.Log ("Player should die");
@@ -21,7 +36,7 @@ public class PlayerHealth : MonoBehaviour
 	public void applyDamage (int damage)
 	{
 		health -= damage;
-		Debug.Log ("Player got hit! Health is " + health);
+		healthBar.setLife (health);
 		if (health <= 0) {
 			die ();
 		}
