f1f773412343ac9cc5dcc319fdf2523308cb8285
ychazallon <windows@gurrcat>
2013-11-27 21:35:20 +0100

    added missing files

---
+++ prototype/Assets/Scripts/Weapons/MachineGun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MachineGun : AbstractWeapon
+{
+
+    public float timeBetweenShots = 0.2f;
+    private float delay = 0;
+
+    public void Update()
+    {
+        base.Update();
+        delay -= Time.deltaTime;
+    }
+
+    public override void triggerDown()
+    {
+    }
+
+    public override void triggerHold()
+    {
+        if (delay <= 0)
+        {
+            Debug.Log("FIRE");
+            delay = timeBetweenShots;
+            fire();
+        }
+    }
+
+    public override void triggerUp()
+    {
+    }
+}
