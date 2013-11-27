6a4a41425256a7fe4ecdcd72be97a8fe65b2cb4a
ychazallon <windows@wynock>
2013-11-27 16:36:59 +0100

    weapon picking with E. Lack some comments, and proper weapon placement

---
+++ prototype/Assets/Scripts/Player/PlayerGuns.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGuns : MonoBehaviour
+{
+	public float MaximumPickupRange = 3;
+
+    private static GameObject ragdollWeapons;
+    private static GameObject currentGun;
+
+    void Awake()
+    {
+        ragdollWeapons = GameObject.FindGameObjectWithTag("RagdollWeaponsContainer");
+        currentGun = transform.FindChild("Gun").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			//looking for the closest weapon within range
+			Transform closestWeapon = null;
+			float bestDistance = MaximumPickupRange;
+			foreach (Transform otherWeapon in ragdollWeapons.transform)
+			{
+				float distance = Vector3.Distance(transform.position, otherWeapon.transform.position);
+				if (distance < bestDistance)
+				{
+					bestDistance = distance;
+					closestWeapon = otherWeapon;
+				}
+			}
+
+
+			//if found, switch
+			if (closestWeapon != null)
+			{
+				pickWeapon(closestWeapon.gameObject);
+			}
+		}
+    }
+
+	private void pickWeapon(GameObject newWeapon)
+	{
+		AbstractWeapon newWeaponScript = newWeapon.GetComponent<AbstractWeapon>();
+		if (newWeaponScript.Mode == WeaponMode.RAGDOLL)
+		{
+			//set new weapon transform so that it's handled where the old one was
+			newWeapon.transform.parent = this.transform;
+			newWeapon.transform.localPosition = Vector3.zero;
+			newWeapon.transform.localRotation = Quaternion.identity;
+			newWeaponScript.Mode = WeaponMode.HAND;
+
+			//drop current weapon
+			currentGun.transform.parent = ragdollWeapons.transform;
+			currentGun.GetComponent<AbstractWeapon>().Mode = WeaponMode.RAGDOLL;
+
+			currentGun = newWeapon;
+			
+		}
+	}
+
+    private void dropCurrentWeapon()
+    {
+		
+    }
+}
--- prototype/Assets/Scripts/Weapons/AbstractWeapon.cs
+++ prototype/Assets/Scripts/Weapons/AbstractWeapon.cs
@@ -1,101 +1,111 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum WeaponMode
+{
+	/// <summary>
+	/// weapon is handled by the physics engine and is pickable
+	/// </summary>
+	RAGDOLL,
+
+	/// <summary>
+	/// weapon is carried by a player but won't fire
+	/// </summary>
+	HOLSTER,
+
+	/// <summary>
+	///  weapon is carried by a player and will kill zombies :)
+	/// </summary>
+	HAND
+}
 
 public abstract class AbstractWeapon : MonoBehaviour
 {
-    public enum WeaponMode
-    {
-        /// <summary>
-        /// weapon is handled by the physics engine and is pickable
-        /// </summary>
-        RAGDOLL,
 
-        /// <summary>
-        /// weapon is carried by a player but won't fire
-        /// </summary>
-        HOLSTER,
+	public Transform gunMuzzle;
 
-        /// <summary>
-        ///  weapon is carried by a player and will kill zombies :)
-        /// </summary>
-        HAND
-    }
+	public bool initAsRagdoll;
 
-    public Transform gunMuzzle;
+	private WeaponMode mode;
+	public WeaponMode Mode
+	{
+		get { return mode; }
+		set
+		{
+			this.mode = value;
+			switch (mode)
+			{
+				case WeaponMode.RAGDOLL:
+					{
+						rigidbody.isKinematic = false;
+						collider.enabled = true;
+						break;
+					}
+				case WeaponMode.HOLSTER:
+					{
+						rigidbody.isKinematic = true;
+						collider.enabled = false;
+						break;
+					}
+				case WeaponMode.HAND:
+					{
+						rigidbody.isKinematic = true;
+						collider.enabled = false;
+						break;
+					}
+			}
+		}
+	}
 
-    public bool initAsRagdoll;
-    private WeaponMode mode;
-    public WeaponMode Mode
-    {
-        get { return mode; }
-        set
-        {
-            this.mode = value;
-            switch(mode)
-            {
-                case WeaponMode.RAGDOLL:
-                {
-                    rigidbody.isKinematic = false;
-                    collider.enabled = true;
-                    break;
-                }
-                case WeaponMode.HOLSTER:
-                {
-                    rigidbody.isKinematic = true;
-                    collider.enabled = false;
-                    break;
-                }
-                case WeaponMode.HAND:
-                {
-                    rigidbody.isKinematic = true;
-                    collider.enabled = false;
-                    break;
-                }
-            }
-        }
-    }
+	void Start()
+	{
+		if (initAsRagdoll)
+			Mode = WeaponMode.RAGDOLL;
+		else
+			Mode = WeaponMode.HAND;     //default is weapon Handed (may be changed later)
 
-    void Start()
-    {
-        if (initAsRagdoll)
-            Mode = WeaponMode.RAGDOLL;
-        else
-            Mode = WeaponMode.HAND;     //default is weapon Handed (may be changed later)
+		if (gunMuzzle == null)
+		{
+			Debug.Log("Weapon muzzle is not set");
+		}
+	}
 
-        if (gunMuzzle == null)
-        {
-            Debug.Log("Weapon muzzle is not set");
-        }
-    }
+	//void OnMouseDown()
+	//{
+	//	if (mode == WeaponMode.RAGDOLL)
+	//	{
+	//		PlayerGuns playerGunScript = FindObjectOfType<PlayerGuns>();
+	//		playerGunScript.pickWeapon(gameObject);
+	//	}
+	//}
 
-    void Update()
-    {
-        if (mode == WeaponMode.HAND)
-        {
-            //changing this bloc allow to change the way to fire all weapons
-            if (Input.GetButtonDown("Fire1"))
-                triggerDown();
-            else if (Input.GetButtonUp("Fire1"))
-                triggerUp();
+	void Update()
+	{
+		if (mode == WeaponMode.HAND)
+		{
+			//changing this bloc allow to change the way to fire all weapons
+			if (Input.GetButtonDown("Fire1"))
+				triggerDown();
+			else if (Input.GetButtonUp("Fire1"))
+				triggerUp();
 
-            if (Input.GetButton("Fire1"))
-                triggerHold();
-        }
-    }
+			if (Input.GetButton("Fire1"))
+				triggerHold();
+		}
+	}
 
 
-    /// <summary>
-    /// Should be called by the sub-class (specific weapon) whenever it wants to instantiate a projectile
-    /// </summary>
-    protected void fire()
-    {
-        //TODO : handle different projectile types?
-        Rigidbody projectile = ProjectileFactory.sharedFactory().deliverProjectile(gunMuzzle);
-    }
+	/// <summary>
+	/// Should be called by the sub-class (specific weapon) whenever it wants to instantiate a projectile
+	/// </summary>
+	protected void fire()
+	{
+		//TODO : handle different projectile types?
+		Rigidbody projectile = ProjectileFactory.sharedFactory().deliverProjectile(gunMuzzle);
+	}
 
-    public abstract void triggerDown();
-    public abstract void triggerHold();
-    public abstract void triggerUp();
+	public abstract void triggerDown();
+	public abstract void triggerHold();
+	public abstract void triggerUp();
 
 }
