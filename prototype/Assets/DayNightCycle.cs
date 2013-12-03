d4ed1dc5706398fcce0bd787223481afc1e60fc5
ychazallon <windows@gurrcat>
2013-12-03 02:09:00 +0100

    added day/night cycle

---
+++ prototype/Assets/DayNightCycle.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DayNightCycle : MonoBehaviour
+{
+    public const float SUMMER_INCLINATION = 90;
+    public const float WINTER_INCLINATION = 25;
+
+    [Range(WINTER_INCLINATION, SUMMER_INCLINATION)]
+    public float summerness = 50;
+    public float dayDurationSeconds = 120;
+    public float nightDurationSeconds = 60;
+
+    private float dayTimer;
+    private bool isDay;
+    public bool IsDay
+    {
+        get { return isDay; }
+    }
+
+    private Light sun;
+
+    // Use this for initialization
+    void Start()
+    {
+        dayTimer = dayDurationSeconds / 2;  //starts at noon
+        isDay = true;
+        sun = GetComponentInChildren<Light>();
+        sun.transform.localRotation = Quaternion.identity;
+        transform.rotation = Quaternion.Euler(summerness,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsDay)
+        {
+            dayTimer += Time.deltaTime;
+            sun.transform.localRotation = Quaternion.Euler(0, 180 * dayProgress() - 90, 0);
+
+            if (dayTimer > dayDurationSeconds)
+            {
+                StartCoroutine(night());
+            }
+        }
+    }
+
+    public float dayProgress()
+    {
+        return (dayTimer / dayDurationSeconds); 
+    }
+
+    private IEnumerator night()
+    {
+        sun.enabled = false;
+        isDay = false;
+        yield return new WaitForSeconds(nightDurationSeconds);
+        sun.enabled = true;
+        isDay = true;
+        dayTimer = 0;
+    }
+}
