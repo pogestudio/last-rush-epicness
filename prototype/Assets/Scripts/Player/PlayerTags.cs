d1703e744a03bbedb1a04a86bcc0f5e08a2f0ffa
ychazallon <windows@gurrcat>
2013-12-08 18:37:11 +0100

    added moonlight. Fixed player replicas tag to OtherPlayer

---
+++ prototype/Assets/Scripts/Player/PlayerTags.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTags : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (networkView.isMine)
+        {
+            tag = "Player";
+        }
+        else
+        {
+            tag = "OtherPlayer";
+        }
+
+        this.enabled = false;
+	}
+}
