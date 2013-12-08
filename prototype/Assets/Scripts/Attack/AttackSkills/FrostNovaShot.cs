6f203d57086df53027f2b2fda3e2eff9e0ee2265
CA Ewerbring <caewerbring@gmail.com>
2013-12-08 17:54:48 +0100

    Added skill menu

--- prototype/Assets/Scripts/Attack/AttackSkills/FrostNovaShot.cs
+++ prototype/Assets/Scripts/Attack/AttackSkills/FrostNovaShot.cs
@@ -3,36 +3,37 @@ using System.Collections;
 
 public class FrostNovaShot : SkillEffect
 {
-	private float novaDistance = 5F;
-	private float slowingSpeed = 0.3F;
-	private float slowingDuration = 5F;
+		private float novaDistance = 5F;
+		private float slowingSpeed = 0.3F;
+		private float slowingDuration = 5F;
 
-	public override void doDamage (GameObject colliderObject)
-	{
-		ArrayList monstersWithinNova = MonsterFinder.sharedHelper ().monstersWithinArea (transform.position, novaDistance);
-		foreach (GameObject monster in monstersWithinNova) {
-			SlowingEffect existingEffect = monster.GetComponent<SlowingEffect> ();
+		public override void doDamage (GameObject colliderObject)
+		{
+				Debug.Log ("Nova fired!");
+				ArrayList monstersWithinNova = MonsterFinder.sharedHelper ().monstersWithinArea (transform.position, novaDistance);
+				foreach (GameObject monster in monstersWithinNova) {
+						SlowingEffect existingEffect = monster.GetComponent<SlowingEffect> ();
 			
-			//if they are slowed already, and existing effect is stronger, skip
-			if (existingEffect != null && existingEffect.slowingPercentage > slowingSpeed) {
-				continue;
-			}
+						//if they are slowed already, and existing effect is stronger, skip
+						if (existingEffect != null && existingEffect.slowingPercentage > slowingSpeed) {
+								continue;
+						}
 			
-			if (existingEffect != null) {
-				existingEffect.slowingDuration = slowingDuration;
-				existingEffect.updateTimer ();
-			} else {
+						if (existingEffect != null) {
+								existingEffect.slowingDuration = slowingDuration;
+								existingEffect.updateTimer ();
+						} else {
 			
-				//if not just add on. 
-				SlowingEffect newEffect = monster.AddComponent<SlowingEffect> ();
-				newEffect.slowingDuration = slowingDuration;
-				newEffect.slowingPercentage = slowingSpeed;
-			}
+								//if not just add on. 
+								SlowingEffect newEffect = monster.AddComponent<SlowingEffect> ();
+								newEffect.slowingDuration = slowingDuration;
+								newEffect.slowingPercentage = slowingSpeed;
+						}
+				}
 		}
-	}
 
-	public override void createEffect (GameObject colliderObject)
-	{
-		EffectFactory.sharedFactory ().deliverSphericNova (transform.position);
-	}
+		public override void createEffect (GameObject colliderObject)
+		{
+				EffectFactory.sharedFactory ().deliverSphericNova (transform.position);
+		}
 }
---
+++ prototype/Assets/Scripts/GUI/WeaponSkillMenu.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+public class WeaponSkillMenu : MonoBehaviour
+{
+
+		private bool menuIsOpen;
+		private bool skillInfoIsOpen;
+		private int indexOfChosenSkill;
+		public GUISkin toolTipSkin;
+		
+		int WSx = 50;
+		int WSy = 50;
+		int WSWidth = 200;
+	
+		
+		
+		int SIWidth = 200;
+		int XPadding = 10;
+		// Use this for initialization
+		void Start ()
+		{
+				WSx = Screen.width - XPadding * 2 - WSWidth - SIWidth;
+		}
+	
+		void Update ()
+		{
+				if (Input.GetKeyDown ("n") && menuIsOpen) {
+						closeMenu ();
+				} else if (Input.GetKeyDown ("n") && !menuIsOpen) {
+						openMenu ();
+				}
+		}
+		
+		void openMenu ()
+		{
+				Debug.Log ("Will open menu");
+				menuIsOpen = true;
+		}
+		
+		void closeMenu ()
+		{
+				Debug.Log ("Will close menu");
+				menuIsOpen = false;
+				skillInfoIsOpen = false;
+		}
+		
+		void OnGUI ()
+		{
+				if (menuIsOpen) {
+						drawWeaponSkillMenu ();
+				}
+				if (skillInfoIsOpen) {
+						drawSkillInfo ();
+				}
+		}
+		
+		void drawWeaponSkillMenu ()
+		{
+		
+				int WSHeight = 50;
+				int WSYPadding = 20;
+				//simply draw the same amount of boxes as we have lives.
+				
+				int currentY = WSy;
+				ArrayList allSkills = Skill.allSkills;
+				for (int skillIndex = 0; skillIndex < allSkills.Count; skillIndex++) {
+						Skill aSkill = (Skill)allSkills [skillIndex];
+						
+						string weaponLevelInfo = "Level: " + aSkill.currentSkillLevel + " :: " + aSkill.chanceToAddEffect () * 100 + "%";
+						string toPresent = SplitCamelCase (aSkill.componentName) + "\n" + SplitCamelCase (aSkill.currentWeaponType.ToString ());
+						toPresent = toPresent + "\n" + weaponLevelInfo;
+			
+						if (GUI.Button (new Rect (WSx, currentY, WSWidth, WSHeight), toPresent, toolTipSkin.button)) {
+								skillInfoIsOpen = true;
+								indexOfChosenSkill = skillIndex;
+						}
+						currentY += WSHeight + WSYPadding;
+						
+				}
+		}
+		
+		void drawSkillInfo ()
+		{
+				Skill skillToDraw = (Skill)Skill.allSkills [indexOfChosenSkill];
+				int SIx = WSx + WSWidth + XPadding;
+				int SIy = WSy;
+				int SIHeight = 300;
+				string toPresent = SplitCamelCase (skillToDraw.componentName) + "\n" + skillToDraw.description;
+				GUI.Box (new Rect (SIx, SIy, SIWidth, SIHeight), toPresent, toolTipSkin.box);
+		}
+		
+		string SplitCamelCase (string str)
+		{
+				return Regex.Replace (
+		                     Regex.Replace (
+		              str, 
+		              @"(\P{Ll})(\P{Ll}\p{Ll})", 
+		              "$1 $2" 
+				), 
+		                     @"(\p{Ll})(\P{Ll})", 
+		                     "$1 $2" 
+				);
+		}
+	
+}
