89d10da9cd0256ad2c035fa07a111296a9a5b3b3
CA Ewerbring <caewerbring@gmail.com>
2013-12-09 13:39:49 +0100

    Changed gatling gun dps

--- prototype/Assets/Scripts/Weapons/AbstractWeapon.cs
+++ prototype/Assets/Scripts/Weapons/AbstractWeapon.cs
@@ -3,98 +3,108 @@ using System.Collections;
 
 public enum WeaponMode
 {
-	/// <summary>
-	/// weapon is handled by the physics engine and is pickable
-	/// </summary>
-	RAGDOLL,
+		/// <summary>
+		/// weapon is handled by the physics engine and is pickable
+		/// </summary>
+		RAGDOLL,
 
-	/// <summary>
-	/// weapon is carried by a player but won't fire
-	/// </summary>
-	HOLSTER,
+		/// <summary>
+		/// weapon is carried by a player but won't fire
+		/// </summary>
+		HOLSTER,
 
-	/// <summary>
-	///  weapon is carried by a player and will kill zombies :)
-	/// </summary>
-	HAND
+		/// <summary>
+		///  weapon is carried by a player and will kill zombies :)
+		/// </summary>
+		HAND
 }
 
 public abstract class AbstractWeapon : MonoBehaviour
 {	
-	public Transform gunMuzzle;
-	public WeaponTypes thisType;
+		public Transform gunMuzzle;
+		public WeaponTypes thisType;
 	
-	private float despawnDelay = 20F; //if it hasnt been picked up in 20, despawn.
-	private float despawnSafety = (float)60 * 60 * 2; //incremetn despawn time with this if you pick it up. Should be a lot, like two hours
-	private float timeToDespawn;
+		private float despawnDelay = 20F; //if it hasnt been picked up in 20, despawn.
+		private float despawnSafety = (float)60 * 60 * 2; //incremetn despawn time with this if you pick it up. Should be a lot, like two hours
+		private float timeToDespawn;
 	
-	public float timeBetweenShots;
-	public int bulletSpeed;
-	public int weaponDamage;
+		public float timeBetweenShots;
+		public int bulletSpeed;
+		public int weaponDamage;
 
-	protected WeaponMode mode;
-	public WeaponMode Mode {
-		get { return mode; }
-		set {
-			this.mode = value;
-			switch (mode) {
-			case WeaponMode.RAGDOLL:
-				{
-					rigidbody.isKinematic = false;
-					collider.enabled = true;
-					timeToDespawn = Time.time + despawnDelay;
-					break;
+		protected WeaponMode mode;
+		public WeaponMode Mode {
+				get { return mode; }
+				set {
+						this.mode = value;
+						switch (mode) {
+						case WeaponMode.RAGDOLL:
+								{
+										rigidbody.isKinematic = false;
+										collider.enabled = true;
+										timeToDespawn = Time.time + despawnDelay;
+										break;
+								}
+						case WeaponMode.HOLSTER:
+								{
+										rigidbody.isKinematic = true;
+										collider.enabled = false;
+										timeToDespawn = Time.time + despawnSafety;
+										break;
+								}
+						case WeaponMode.HAND:
+								{
+										rigidbody.isKinematic = true;
+										collider.enabled = false;
+										timeToDespawn = Time.time + despawnSafety;
+										break;
+								}
+						}
 				}
-			case WeaponMode.HOLSTER:
-				{
-					rigidbody.isKinematic = true;
-					collider.enabled = false;
-					timeToDespawn = Time.time + despawnSafety;
-					break;
+		}
+
+		protected void Awake ()
+		{
+				if (gunMuzzle == null) {
+						Debug.Log ("Weapon muzzle is not set");
 				}
-			case WeaponMode.HAND:
-				{
-					rigidbody.isKinematic = true;
-					collider.enabled = false;
-					timeToDespawn = Time.time + despawnSafety;
-					break;
+				if (!gameObject.activeInHierarchy) {
+						Debug.Log ("The weapon is deactive in hierarchy...?");
 				}
-			}
+				WeaponManager.get ().manage (this);
 		}
-	}
 
-	protected void Awake ()
-	{
-		if (gunMuzzle == null) {
-			Debug.Log ("Weapon muzzle is not set");
+
+		public void Start ()
+		{
 		}
-		if (!gameObject.activeInHierarchy) {
-			Debug.Log ("The weapon is deactive in hierarchy...?");
+
+		public void Update ()
+		{
+				if (mode == WeaponMode.RAGDOLL && Time.time > timeToDespawn) {
+						Destroy (gameObject);
+				}
 		}
-		WeaponManager.get().manage(this);
-	}
 
 
-	public void Start ()
-	{
-	}
+		/// <summary>
+		/// Should be called by the sub-class (specific weapon) whenever it wants to instantiate a projectile
+		/// </summary>
+		public abstract void fire ();
 
-	public void Update ()
-	{
-		if (mode == WeaponMode.RAGDOLL && Time.time > timeToDespawn) {
-			Destroy (gameObject);
+		public abstract void triggerDown ();
+		public abstract void triggerHold ();
+		public abstract void triggerUp ();
+	
+		/// <summary>
+		/// This should return weaponspeed, with consideration to "cooling effects" and stuff for gatling gun.
+		/// Will be overwritten in subclasses where it matters.
+		/// </summary>
+		/// <returns>The weapon speed.</returns>
+		public float normalizedWeaponSpeed ()
+		{
+				return timeBetweenShots;
 		}
-	}
-
-
-	/// <summary>
-	/// Should be called by the sub-class (specific weapon) whenever it wants to instantiate a projectile
-	/// </summary>
-	public abstract void fire ();
-
-	public abstract void triggerDown ();
-	public abstract void triggerHold ();
-	public abstract void triggerUp ();
 	
 	
 
