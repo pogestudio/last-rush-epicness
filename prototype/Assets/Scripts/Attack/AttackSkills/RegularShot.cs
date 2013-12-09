3d59164b5ce645d0bc1929a6f6e54e4b9c9b7411
ychazallon <windows@gurrcat>
2013-12-10 00:29:29 +0100

    factorised blood into effect factory + online sync

--- prototype/Assets/Scripts/Attack/AttackSkills/RegularShot.cs
+++ prototype/Assets/Scripts/Attack/AttackSkills/RegularShot.cs
@@ -4,15 +4,16 @@ using System.Collections;
 public class RegularShot : SkillEffect
 {
 
-	public bool wasTriggeredByMultishot;
+    public bool wasTriggeredByMultishot;
 
-	public override void doDamage (GameObject colliderObject)
-	{
-		doDamageToSingleTarget (colliderObject, baseShotDamage, currentWeaponType);
-	}
-	
-	public override void createEffect (GameObject colliderObject)
-	{
-	
-	}
+    public override void doDamage(GameObject colliderObject)
+    {
+        doDamageToSingleTarget(colliderObject, baseShotDamage, currentWeaponType);
+    }
+
+    public override void createEffect(GameObject colliderObject)
+    {
+        Vector3 direction = colliderObject.transform.position - transform.position;
+        EffectFactory.sharedFactory().deliverBloodSpurtEffect(colliderObject.transform.position, direction);
+    }
 }
